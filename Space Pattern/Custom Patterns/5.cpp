da160dd75778aaf9b8bc4af62ba3e4d52618d7cc
Sujal Jagdev <rg2844537@gmail.com>
2025-02-19 14:40:59 +0530

    Add new space pattern programs and remove old implementations

---
+++ Space Pattern/Custom Patterns/5.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    for (int i = 5; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        for (int k = 5; k > i; k--)
+        {
+            cout << "    ";
+        }
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i = 2; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        for (int k = 5; k > i; k--)
+        {
+            cout << "    ";
+        }
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
